1c04129daf5ae659986fb69762d70aa5e8651e4b
JavedALiShamma <153979997+JavedALiShamma@users.noreply.github.com>
2024-01-25 00:25:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(event) {
+    // event.preventDefault();
+    // HERE WE have already in the form
+    let fname=document.querySelector("#first-name");
+    let lname=document.querySelector("#last-name");
+    alert(fname.value +" "+ lname.value );
 }
